8752e609970c47bf8a5e81f4ec281e189d87dfa9
Tharunvikas55 <tharunvikasss.20msc@kongu.edu>
2024-09-13 14:50:04 +0530

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,8 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    //origin: "http://localhost:3000",
+    origin: "https://expensetracker-backend-jk4b.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
