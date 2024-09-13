371d5a68e1bd2a5e468ab8d531e4927d8274f9d4
Tharunvikas55 <tharunvikasss.20msc@kongu.edu>
2024-09-13 15:19:08 +0530

    added port

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ import path from "path";
 dotenv.config({ path: "./config/config.env" });
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT||4000;
 
 connectDB();
 
